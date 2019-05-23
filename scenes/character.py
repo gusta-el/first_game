b6c36f1f6788c64064253bd6e4c5a0cc1f02c013
Rotciv Ocnarb <rotciv_ocnarb@hotmail.com>
2019-05-23 10:44:47 -0300

    Switch de Personagem

--- scenes/character.py
+++ scenes/character.py
@@ -5,9 +5,11 @@ from physics.body import Body
 
 class Player(Body):
 
-    def __init__(self):
-        super().__init__(Vector2(0, 0), 'rect', pygame.Vector2(50, 50), 'dynamic')
+    def __init__(self, position):
+        super().__init__(position, 'rect', pygame.Vector2(50, 50), 'dynamic')
         self.texture = pygame.image.load("res/character.png")
+
+        self.control = False
 
         self.speed = 5
         self.hor = 0
@@ -16,49 +18,51 @@ class Player(Body):
 
     def render(self, renderer):
 
-        renderer.camera_pos += (self.position - renderer.camera_pos) / 5
+        if self.control:
+            renderer.camera_pos += (self.position - renderer.camera_pos) / 5
 
         renderer.drawTexture(self.texture, self.position.x, self.position.y, 64, 64)
         super().render(renderer)
 
     def update(self, delta):
-        if self.hor != 0 or self.ver != 0:
+        if (self.hor != 0 or self.ver != 0) and self.control:
             tvel = Vector2(self.hor, self.ver).normalize() * self.speed
         else: tvel = Vector2(0, 0)
 
         self.velocity += (tvel - self.velocity) / 5
 
     def input(self, event):
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_LEFT:
-                self.hor = -1
-            elif event.key == pygame.K_RIGHT:
-                self.hor = 1
-            elif event.key == pygame.K_UP:
-                self.ver = -1
-            elif event.key == pygame.K_DOWN:
-                self.ver = 1
-
-        if event.type == pygame.KEYUP:
-            if event.key == pygame.K_LEFT:
-                if pygame.key.get_pressed()[pygame.K_RIGHT]:
-                    self.hor = 1
-                else:
-                    self.hor = 0
-            elif event.key == pygame.K_RIGHT:
-                if pygame.key.get_pressed()[pygame.K_LEFT]:
+        if(self.control):
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_LEFT:
                     self.hor = -1
-                else:
-                    self.hor = 0
-            elif event.key == pygame.K_UP:
-                if pygame.key.get_pressed()[pygame.K_DOWN]:
+                elif event.key == pygame.K_RIGHT:
+                    self.hor = 1
+                elif event.key == pygame.K_UP:
+                    self.ver = -1
+                elif event.key == pygame.K_DOWN:
                     self.ver = 1
-                else:
-                    self.ver = 0
-            elif event.key == pygame.K_DOWN:
-                if pygame.key.get_pressed()[pygame.K_UP]:
-                    self.ver = 1
-                else:
-                    self.ver = 0
+
+            if event.type == pygame.KEYUP:
+                if event.key == pygame.K_LEFT:
+                    if pygame.key.get_pressed()[pygame.K_RIGHT]:
+                        self.hor = 1
+                    else:
+                        self.hor = 0
+                elif event.key == pygame.K_RIGHT:
+                    if pygame.key.get_pressed()[pygame.K_LEFT]:
+                        self.hor = -1
+                    else:
+                        self.hor = 0
+                elif event.key == pygame.K_UP:
+                    if pygame.key.get_pressed()[pygame.K_DOWN]:
+                        self.ver = 1
+                    else:
+                        self.ver = 0
+                elif event.key == pygame.K_DOWN:
+                    if pygame.key.get_pressed()[pygame.K_UP]:
+                        self.ver = 1
+                    else:
+                        self.ver = 0
         pass
 
--- scenes/defaultScene.py
+++ scenes/defaultScene.py
@@ -8,23 +8,35 @@ from physics.body import Body
 class DefaultScene(Scene):
 
     def start(self):
-        self.character = Player()
+        self.character1 = Player(pygame.Vector2(0, 0))
+        self.character2 = Player(pygame.Vector2(0, 100))
         self.world = World(Vector2(0, 0))
 
-        self.world.addBody(self.character)
+        self.character1.control = True
+
+        self.world.addBody(self.character1)
+        self.world.addBody(self.character2)
         self.world.addBody(Body(pygame.Vector2(300, 300), 'rect', pygame.Vector2(60, 40), 'static'))
         pass
 
     def input(self, event):
-        self.character.input(event)
+        self.character1.input(event)
+        self.character2.input(event)
+
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_SPACE:
+                self.character1.control = not self.character1.control
+                self.character2.control = not self.character2.control
         pass
 
     def update(self, delta):
-        self.character.update(delta)
+        self.character1.update(delta)
+        self.character2.update(delta)
         self.world.update(delta)
         pass
 
     def render(self, renderer):
-        self.character.render(renderer)
+        self.character1.render(renderer)
+        self.character2.render(renderer)
         self.world.render(renderer)
         pass
